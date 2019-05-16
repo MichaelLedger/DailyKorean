6456c0dcd1d78caf4200d79b296766d30507096d
Mountain Xiang <783600983@qq.com>
2019-05-16 13:12:33 +0800

    主结构Tabbar初始化

--- DailyKorean/DailyKorean/AppDelegate.swift
+++ DailyKorean/DailyKorean/AppDelegate.swift
@@ -14,9 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        window!.backgroundColor = UIColor.white
+        window!.makeKeyAndVisible()
+        
+        let tabbar = DKTabBarController()
+        window!.rootViewController = tabbar
+        
         return true
     }
 
---
+++ DailyKorean/DailyKorean/DKTabBarController.swift
@@ -0,0 +1,51 @@
+//
+//  DKTabBarController.swift
+//  DailyKorean
+//
+//  Created by MountainX on 2019/5/16.
+//  Copyright © 2019 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class DKTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setup()
+    }
+    
+    func setup() {
+        let vc1 = DKBaseViewController()
+        vc1.view.backgroundColor = UIColor.orange
+        vc1.tabBarItem = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 1)
+        
+        let vc2 = DKBaseViewController()
+        vc2.view.backgroundColor = UIColor.yellow
+        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 2)
+        
+        let vc3 = DKBaseViewController()
+        vc3.view.backgroundColor = UIColor.gray
+        vc3.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 3)
+        
+        let vc4 = DKBaseViewController()
+        vc4.view.backgroundColor = UIColor.magenta
+        vc4.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 4)
+        
+        viewControllers = [vc1, vc2, vc3, vc4]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
