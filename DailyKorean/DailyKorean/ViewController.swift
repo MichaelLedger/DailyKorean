45f9f0dc9ee3eb566614c9bc3cd2096f24c30400
Mountain Xiang <783600983@qq.com>
2019-05-16 12:44:22 +0800

    工程初始化创建，最低支持iOS10

---
+++ DailyKorean/DailyKorean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyKorean
+//
+//  Created by MountainX on 2019/5/16.
+//  Copyright © 2019 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
